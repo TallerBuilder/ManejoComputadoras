1c52093d16ffcd4175c3997dba1926e7dfe65769
jreyesr <jreyesr@users.noreply.github.com>
2018-12-11 12:16:57 -0500

    Definir interfaz ComputadoraBuilder

--- ManejoComputadoras/src/emsamablajecomputadoras/builder/ComputadoraBuilder.java
+++ ManejoComputadoras/src/emsamablajecomputadoras/builder/ComputadoraBuilder.java
@@ -1,5 +1,12 @@
 package emsamablajecomputadoras.builder;
 
 public interface ComputadoraBuilder {
-
+	public void definirComputadora();
+	
+	public void construirAlmacenamiento();
+	public void construirMainboard();
+	public void construirRAM();
+	public void setExtras();
+	
+	public Computadora getComputadora();
 }
