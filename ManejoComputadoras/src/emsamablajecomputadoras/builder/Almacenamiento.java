5628fb8e13cc4be73d48782e603c91956068be68
jreyesr <jreyesr@users.noreply.github.com>
2018-12-11 12:26:46 -0500

    Implementar clases componentes

--- ManejoComputadoras/src/emsamablajecomputadoras/builder/Almacenamiento.java
+++ ManejoComputadoras/src/emsamablajecomputadoras/builder/Almacenamiento.java
@@ -1,5 +1,27 @@
 package emsamablajecomputadoras.builder;
 
 public class Almacenamiento {
+	private int capacidadGB;
+	private String tipo;
+	
+	public Almacenamiento(int capacidad, String tipo) {
+		this.capacidadGB=capacidad;
+		this.tipo=tipo;
+	}
 
+	public int getCapacidadGB() {
+		return capacidadGB;
+	}
+
+	public void setCapacidadGB(int capacidadGB) {
+		this.capacidadGB = capacidadGB;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
 }
--- ManejoComputadoras/src/emsamablajecomputadoras/builder/RAM.java
+++ ManejoComputadoras/src/emsamablajecomputadoras/builder/RAM.java
@@ -1,5 +1,29 @@
 package emsamablajecomputadoras.builder;
 
 public class RAM {
+    private int capacidadGB;
+    private String tipo;
+    
+	public RAM(int capacidadGB, String tipo) {
+		this.capacidadGB = capacidadGB;
+		this.tipo = tipo;
+	}
 
+	public int getCapacidadGB() {
+		return capacidadGB;
+	}
+
+	public void setCapacidadGB(int capacidadGB) {
+		this.capacidadGB = capacidadGB;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+    
+    
 }
