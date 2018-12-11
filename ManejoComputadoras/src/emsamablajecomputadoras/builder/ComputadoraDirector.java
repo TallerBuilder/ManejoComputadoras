bb43748c6d08666940e613e95c4561641b67cfa4
jreyesr <jreyesr@users.noreply.github.com>
2018-12-11 12:46:21 -0500

    Implementar ComputadoraDirector

--- ManejoComputadoras/src/emsamablajecomputadoras/builder/ComputadoraDirector.java
+++ ManejoComputadoras/src/emsamablajecomputadoras/builder/ComputadoraDirector.java
@@ -1,5 +1,18 @@
 package emsamablajecomputadoras.builder;
 
 public class ComputadoraDirector {
+	private ComputadoraBuilder builder;
 
+	public ComputadoraDirector(ComputadoraBuilder builder) {
+		this.builder = builder;
+	}
+	
+	public Computadora build() {
+		builder.definirComputadora();
+		builder.construirAlmacenamiento();
+		builder.construirMainboard();
+		builder.construirRAM();
+		builder.construirSO();
+		return builder.getComputadora();
+	}
 }
